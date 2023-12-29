78b24f857156b9d379effe6467929ade62e5fd0a
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-12-29 15:49:30 +0300

    Initial Commit

---
+++ Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by Pavel on 29.12.23.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
