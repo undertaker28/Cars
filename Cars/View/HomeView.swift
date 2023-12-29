08651cf6d31ef26ec77061e1ee0cb0d313a827ac
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-12-29 16:30:05 +0300

    Add Splash View

---
+++ Cars/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Cars
+//
+//  Created by Pavel on 29.12.23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Cars/View/SplashView.swift
@@ -0,0 +1,40 @@
+//
+//  SplashView.swift
+//  Cars
+//
+//  Created by Pavel on 29.12.23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isActive = false
+    
+    var body: some View {
+        if isActive {
+            HomeView()
+        } else {
+            ZStack {
+                Color("Background")
+                    .ignoresSafeArea()
+                
+                    Image("Icon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 160, height: 160, alignment: .center)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.6) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
