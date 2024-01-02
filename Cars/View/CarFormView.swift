40d8351d6d741536bcc37c4dd792e58cceb2ada7
undertaker28 <pavel.starovoytovvv@gmail.com>
2024-01-02 21:34:57 +0300

    Minor fix

--- Cars/View/CarFormView.swift
+++ Cars/View/CarFormView.swift
@@ -27,63 +27,65 @@ struct CarFormView: View {
     }
     
     var body: some View {
-        VStack {
-            CarImageView(uiImage: carFormViewModel.selectedImage)
-                .padding(.top, 20)
-                .padding(.bottom, 15)
-            
-            VStack(alignment: .center, spacing: 26) {
-                CustomTextField(placeHolder: "Name of model", text: $carFormViewModel.model)
+        ZStack {
+            Color("Background")
+                .ignoresSafeArea()
+            VStack {
+                CarImageView(uiImage: carFormViewModel.selectedImage)
+                    .padding(.top, 20)
+                    .padding(.bottom, 15)
                 
-                CustomTextField(placeHolder: "Name of producer", text: $carFormViewModel.producer)
-                
-                YearPicker(selectedDate: $carFormViewModel.selectedDate)
-                
-                ColorPicker("Color", selection: $carFormViewModel.color)
+                VStack(alignment: .center, spacing: 26) {
+                    CustomTextField(placeHolder: "Name of model", text: $carFormViewModel.model)
+                    
+                    CustomTextField(placeHolder: "Name of producer", text: $carFormViewModel.producer)
+                    
+                    YearPicker(selectedDate: $carFormViewModel.selectedDate)
+                    
+                    ColorPicker("Color", selection: $carFormViewModel.color)
+                        .padding()
+                        .fontWeight(.bold)
+                        .background(.purple)
+                        .cornerRadius(15)
+                    
+                    Button("Select Image") {
+                        carFormViewModel.isImagePickerPresented = true
+                    }
                     .padding()
-                    .fontWeight(.bold)
                     .background(.purple)
-                    .cornerRadius(15)
-                
-                Button("Select Image") {
-                    carFormViewModel.isImagePickerPresented = true
-                }
-                .padding()
-                .background(.purple)
-                .cornerRadius(10)
-                .fontWeight(.bold)
-                .sheet(isPresented: $carFormViewModel.isImagePickerPresented) {
-                    ImagePicker(selectedImage: $carFormViewModel.selectedImage)
-                }
-                
-                Button(action: {
-                    carFormViewModel.saveCar(context: managedObjContext)
-                    dismiss()
-                }, label: {
-                    HStack {
-                        Spacer()
-                        Text("Save")
-                        Spacer()
+                    .cornerRadius(10)
+                    .fontWeight(.bold)
+                    .sheet(isPresented: $carFormViewModel.isImagePickerPresented) {
+                        ImagePicker(selectedImage: $carFormViewModel.selectedImage)
                     }
-                })
-                .font(.headline)
-                .padding()
-                .background(.green)
-                .cornerRadius(10)
-            }
-            .padding(.horizontal)
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    VStack {
-                        Text(title)
-                            .bold()
+                    
+                    Button(action: {
+                        carFormViewModel.saveCar(context: managedObjContext)
+                        dismiss()
+                    }, label: {
+                        HStack {
+                            Spacer()
+                            Text("Save")
+                            Spacer()
+                        }
+                    })
+                    .font(.headline)
+                    .padding()
+                    .background(.green)
+                    .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        VStack {
+                            Text(title)
+                                .bold()
+                        }
                     }
                 }
+                .foregroundColor(Color("Element"))
             }
-            .foregroundColor(Color("Element"))
         }
-        .interactiveDismissDisabled()
-        .background(Color("Background"))
     }
 }
